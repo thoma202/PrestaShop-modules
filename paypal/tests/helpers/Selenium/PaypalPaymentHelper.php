a7922e50e9fe37fb2e1924caf735c161fed77307
thoma202 <tbigueres@202-ecommerce.com>
2013-11-25 11:39:57 +0100

    [+] Begin to add paypal integral express solution test

--- paypal/tests/helpers/Selenium/PaypalPaymentHelper.php
+++ paypal/tests/helpers/Selenium/PaypalPaymentHelper.php
@@ -25,4 +25,20 @@ class PaypalPaymentHelper
 		self::$I->click('input[name=confirmation]');
 	}
 
+	static public function payIntegralExpressWithGoodInformations()
+	{
+		$buyer = new PayPalUserConfiguration('buyer_fr');
+
+		self::$I->click('#paypal_process_payment');
+		self::$I->wait(5000);
+
+		self::$I->fillField('#login_email', $buyer->get('login_email'));
+		self::$I->fillField('#login_password', $buyer->get('login_password'));
+		self::$I->click('#submitLogin');
+
+		self::$I->click('#continue_abovefold');
+		self::$I->click('input[name=confirmation]');
+
+	}
+
 }
